40c29e9408c032d42f4104f167621c5e2e49af8a
shaytheboss <156617988+shaytheboss@users.noreply.github.com>
2024-01-20 17:25:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ function playWord() {
     document.getElementById('translationDisplay').textContent = `תרגום: ${translations[word]}`;
 }
 
-// Function to play a random word
+// Function to play again the word
 function playAgain() {
     const words = Object.keys(translations);
     if (words.length === 0) {
@@ -58,6 +58,26 @@ function playAgain() {
     document.getElementById('translationDisplay').textContent = `תרגום: ${translations[word]}`;
 }
 
+
+// Function to show the hebrew translate
+function displayWordInHebrew() {
+    const words = Object.keys(translations);
+    if (words.length === 0) {
+        console.error('No words found in the translations object');
+        return;
+    }
+
+    const randomIndex = Math.floor(Math.random() * words.length);
+    const word = words[randomIndex];
+
+    // Playing the audio
+    var audio = new Audio(`sounds/${word}.mp3`);
+    audio.play();
+
+    // Displaying the word and its translation
+    document.getElementById('wordDisplay').textContent = `מילה באנגלית: ${word}`;
+    document.getElementById('translationDisplay').textContent = `תרגום: ${translations[word]}`;
+}
 
 
 document.getElementById('playWord').addEventListener('click', loadTranslationsAndPlayWord);
